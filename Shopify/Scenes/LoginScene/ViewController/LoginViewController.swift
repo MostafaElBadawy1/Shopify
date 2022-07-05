21a384bd7cd5b21ce746b250af9db6b540ccfff6
MostafaElBadawy1 <81087849+MostafaElBadawy1@users.noreply.github.com>
2022-07-05 18:35:45 +0200

    added: product VC, login VC, Register VC

---
+++ Shopify/Scenes/LoginScene/ViewController/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  Shopify
+//
+//  Created by Mostafa Elbadawy on 05/07/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var PasswordTF: UITextField!
+    @IBAction func loginBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func goToSignUpScreenBtn(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Shopify/Scenes/ProductDetailsScene/CollectionViewCell/ProductDetailsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductDetailsCollectionViewCell.swift
+//  Shopify
+//
+//  Created by Mostafa Elbadawy on 05/07/2022.
+//
+
+import UIKit
+
+class ProductDetailsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var productImgInCV: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
